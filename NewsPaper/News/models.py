958f0cc3a42ad535a56b85e314db9bcec82e71e5
Bobilo <Bobilo@mail.ru>
2023-02-23 23:54:28 +0500

    pre D6.6

---
+++ NewsPaper/News/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    @classmethod
+    def update_rating(cls):
+        authors = Author.objects.all()
+        for author in authors:
+            summ = 0
+            posts_rating = Post.objects.filter(author=author)
+            for post_rate in posts_rating:
+                summ += int(post_rate.rating)
+            summ = summ*3
+            comments_rating = Comment.objects.filter(post__author=author)
+            for comment_rate in comments_rating:
+                summ += int(comment_rate.rating)
+            author_comments = Comment.objects.filter(user_id=author.user_id)
+            for author_comment in author_comments:
+                summ += int(author_comment.rating)
+            author.rating = summ
+            author.save()
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=128, unique=True)
+
+
+class Post(models.Model):
+    
+    article = 'AR'
+    news = 'NW'
+    POSITIONS = [(article, 'Статья'), (news, 'Новость')]
+    
+    header = models.CharField(max_length=128)
+    rating = models.IntegerField(default=0)
+    text = models.TextField()
+    content_type = models.CharField(max_length=2, choices=POSITIONS, default=news)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, through='PostCategory')
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[:127] + "..."
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    com_text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
---
+++ NewsPaper/News/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+# Create your views here.
+from .models import Post
+
+
+class NewsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = 'timestamp'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'newslist.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts'
+
+
+class Article(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'article.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'article'
